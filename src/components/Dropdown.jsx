2273d529adf147ba652f8b7a3ee8df7baa98934c
Hoàng Ngọc Tuấn <hoangngoctuan2611@gmail.com>
2025-04-14 22:04:04 +0700

    fix some bugs, add header to top of homepage

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -8,15 +8,22 @@ import {
 
 export default function Dropdown({ label, options }) {
   return (
-    <Menu className="text-white text-xl w-60 border-none after:border-none before:border-none">
+    <Menu className="text-white text-lg w-60 border-none after:border-none before:border-none">
       <MenuHandler>
-        <Button variant="outlined" className="text-white border-2 border-white">
+        <Button
+          variant="outlined"
+          className="text-white text-lg border-2 border-white"
+        >
           {label}
         </Button>
       </MenuHandler>
-      <MenuList className="bg-[#313035] text-white border-0 overflow-auto overflow-x-hidden dark-scrollbar h-[200px]">
+      <MenuList className="bg-[#313035] text-white w-56 text-lg border-0 overflow-auto overflow-x-hidden dark-scrollbar h-[200px]">
         {options.map((option, index) => (
-          <MenuItem key={index} onClick={option.onClick} className="text-white">
+          <MenuItem
+            key={index}
+            onClick={option.onClick}
+            className="text-white text-lg"
+          >
             {option}
           </MenuItem>
         ))}
