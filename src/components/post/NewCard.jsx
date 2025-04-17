0ca1c587ff087f5e576ac455c6cb422226afda02
duo11 <duong01659818134@gmail.com>
2025-04-17 19:21:49 +0700

    update post

--- src/components/post/NewCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function NewCard({ data }) {
-  return (
-    <div id="card">
-      <h1>{data.new_header}</h1>
-      <img src={data.new_img} alt="" />
-      <div></div>
-    </div>
-  );
-}
--- src/components/post/ProfilePostCard.jsx
+++ src/components/post/ProfilePostCard.jsx
@@ -1,11 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+function createSlug(name) {
+  return name.trim().replace(/\s+/g, "-").replace(/-+/g, "-");
+}
 export default function PostCard({ data }) {
+  const navigate = useNavigate();
   const imageClassName = " h-40 w-32  overflow-hidden rounded-lg";
   const titleClassName = "text-xl text-white";
   const genreClassName = "text-gray-500";
   const contentClassName = "w-full overflow-hidden line-clamp-2 text-white";
 
   return (
-    <div className="w-full flex flex-nowrap items-center gap-8 hover:outline p-2 rounded-md hover:cursor-pointer">
+    <div
+      className="w-full flex flex-nowrap items-center gap-8 hover:outline p-2 rounded-md hover:cursor-pointer"
+      onClick={() => {
+        localStorage.setItem("new_id", data.new_id);
+        navigate(`/news/${encodeURIComponent(createSlug(data.new_header))}`);
+      }}
+    >
       <div className={imageClassName}>
         <img src={data.new_img} className="w-full h-full object-cover" alt="" />
       </div>
