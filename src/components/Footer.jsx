ec534f5e7b0c347a0bd118444ce136b7341bbc3a
Hoàng Ngọc Tuấn <hoangngoctuan2611@gmail.com>
2025-04-23 21:11:02 +0700

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,21 +7,21 @@ export default function Footer() {
           <img src="/ico.png" className="w-10 mr-3" />
           <p className="text-white font-medium text-3xl">Starlight Cinema</p>
         </div>
-        <p className="text-[#5D6A81] font-normal text-xl">
+        <p className="text-[#8da1c5] font-normal text-xl">
           Starlight Cinema Co., Ltd.
         </p>
-        <p className="text-[#5D6A81] font-normal text-xl">
+        <p className="text-[#8da1c5] font-normal text-xl">
           Business Registration Number: 0123456789
         </p>
-        <p className="text-[#5D6A81] font-normal text-xl mb-5">
+        <p className="text-[#8da1c5] font-normal text-xl mb-5">
           Address: Goldsilk Complex, 430 Cau Am, Van Phuc, Ha Dong, Hanoi,
           Vietnam
         </p>
-        <p className="text-[#5D6A81] font-normal hidden lg:text-xl lg:visible">
+        {/* <p className="lg:block lg:text-xl text-[#8da1c5] font-normal hidden">
           Grab your tickets now and dive into an unforgettable cinematic
           adventure! Whether you crave heart-pounding drama or laugh-out-loud
           comedy, the perfect movie awaits!
-        </p>
+        </p> */}
       </div>
 
       <div className="lg:col-span-3 col-span-11 lg:flex flex-col lg:items-center hidden">
@@ -35,31 +35,31 @@ export default function Footer() {
           </p>
           <a
             href="/home"
-            className="text-[#7f92b3] font-light text-xl inline-flex hover:text-purple-400"
+            className="text-[#8da1c5] font-light text-xl inline-flex hover:text-purple-400"
           >
             Homepage
           </a>
           <a
             href="/movie/filter"
-            className="text-[#7f92b3] font-light text-xl inline-flex hover:text-purple-400"
+            className="text-[#8da1c5] font-light text-xl inline-flex hover:text-purple-400"
           >
             Movies
           </a>
           <a
             href="/theater-movie"
-            className="text-[#7f92b3] font-light text-xl inline-flex hover:text-purple-400"
+            className="text-[#8da1c5] font-light text-xl inline-flex hover:text-purple-400"
           >
             Buy Tickets
           </a>
           <a
             href="/theaters"
-            className="text-[#7f92b3] font-light text-xl inline-flex hover:text-purple-400"
+            className="text-[#8da1c5] font-light text-xl inline-flex hover:text-purple-400"
           >
             Theaters
           </a>
           <a
             href="/news"
-            className="text-[#7f92b3] font-light text-xl inline-flex hover:text-purple-400 mb-5"
+            className="text-[#8da1c5] font-light text-xl inline-flex hover:text-purple-400 mb-5"
           >
             News
           </a>
@@ -95,13 +95,13 @@ export default function Footer() {
           <p className="text-white font-medium text-xl mb-2">CONTACT US</p>
           <p
             href="#"
-            className="text-[#7f92b3] text-base lg:text-xl font-light"
+            className="text-[#8da1c5] text-base lg:text-xl font-light"
           >
             starlightsupport@starlight.com
           </p>
           <p
             href="#"
-            className="text-[#7f92b3] text-base lg:text-xl font-light"
+            className="text-[#8da1c5] text-base lg:text-xl font-light"
           >
             +84 919 162 299
           </p>
